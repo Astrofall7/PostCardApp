841e80b38841ffc85881e897f5d09ccaf424b169
Dominique Tillman <dominiquetillman@Dominiques-MacBook-Pro-2.local>
2014-09-13 15:52:18 -0400

    first commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var MessageLabel: UILabel!
+	@IBOutlet weak var NameLabel: UITextField!
+	@IBOutlet weak var MessageInput: UITextField!
+	@IBOutlet weak var theButton: UIButton!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
@@ -21,5 +26,13 @@ class ViewController: UIViewController {
 	}
 
 
+	@IBAction func buttonPressed(sender: UIButton) {
+		MessageLabel.hidden=false;//changing the message label status to false.
+		MessageLabel.text = MessageInput.text;
+		MessageInput.text="";
+		MessageLabel.textColor = UIColor.redColor();MessageInput.resignFirstResponder();
+	
+		theButton.setTitle("Confirmed.", forState: UIControlState.Normal);
+	}
 }
 
